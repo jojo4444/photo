84801ec12e48b24c69888febfe7cecd04a069f4f
Margarita Shunina <44016179+margaritashunina@users.noreply.github.com>
2019-06-28 12:01:21 +0200

    новый алгоритм

--- Algo.cpp
+++ Algo.cpp
@@ -1,79 +1,42 @@
 #include "Algo.hpp"
 
-int intSquare(int x) {
-    return x * x;
+Algo::Algo(){}
+
+void Algo::upload(const sf::Image& content, const sf::Image& style){
+    content_ = content;
+    style_ = style;
 }
 
-bool colorCmp(Color& u, Color& v){
-    return intSquare(u.r) + intSquare(u.g) + intSquare(u.b) <
-           intSquare(v.r) + intSquare(v.g) + intSquare(v.b);
+sf::Image Algo::getResult(){
+    content_.saveToFile("C:/Users/user/Desktop/photo/neural-style-master/content.jpg");
+    style_.saveToFile("C:/Users/user/Desktop/photo/neural-style-master/style.jpg");
+    std::string tmp = "python C:/Users/user/Desktop/photo/neural-style-master/neural_style.py --network C:/Users/user/Desktop/photo/neural-style-master/imagenet-vgg-verydeep-19.mat --content C:/Users/user/Desktop/photo/neural-style-master/content.jpg --styles C:/Users/user/Desktop/photo/neural-style-master/style.jpg --output C:/Users/user/Desktop/photo/neural-style-master/result.jpg";
+    char cmd[tmp.size() + 4];
+    memset(cmd, 0, sizeof cmd);
+
+    for (int i = 0; i < tmp.size(); ++i){
+        cmd[i] = tmp[i];
+    }
+    system(cmd);
+
+    sf::Image img;
+    while (img.loadFromFile("C:/Users/user/Desktop/photo/neural-style-master/result.jpg") == false || isBlack(img)){
+        sf::sleep(sf::seconds(10));
+    }
+
+    return img;
 }
 
+bool Algo::isBlack(const sf::Image& img) const{
+    sf::Color black(0, 0, 0, 255);
 
-Algo::Algo(){
-    assignColorsConvertor();
-}
-
-Algo::Algo(const Image& first, const Image& second){
-    assignColorsConvertor();
-    first_ = first;
-    second_ = second;
-}
-
-void Algo::upload(const Image& first, const Image& second){
-    first_ = first;
-    second_ = second;
-}
-
-vector <Color> Algo::calculate(const Image& T){
-    vector <Color> res;
-
-    for (int y = 0; y < T.getSize().y; ++y) {
-        for (int x = 0; x < T.getSize().x; ++x) {
-            res.push_back(T.getPixel(x, y));
+    for (int y = 0; y < img.getSize().y; ++y){
+        for (int x = 0; x < img.getSize().x; ++x){
+            if (img.getPixel(x, y) != black){
+                return false;
+            }
         }
     }
 
-    std::sort(res.begin(), res.end(), colorCmp);
-    return res;
-
-}
-
-void Algo::assignColorsConvertor(){
-    for (int i = 0; i < 256; ++i){
-        vector <vector <int> > tmp1;
-        for (int j = 0; j < 256; ++j){
-            vector <int> tmp2;
-            tmp2.assign(256, 0);
-            tmp1.push_back(tmp2);
-        }
-        convertColors_.push_back(tmp1);
-    }
-}
-
-Color Algo::findColor(Color u){
-    int szSource = firstColors_.size();
-    int szFilter = secondColors_.size();
-    int it = (long long)convertColors_[u.r][u.g][u.b] * szFilter / szSource;
-    return secondColors_[it];
-}
-
-Image Algo::getResult(){
-    firstColors_ = calculate(first_);
-    secondColors_ = calculate(second_);
-
-    int num = 0;
-    for (Color& c : firstColors_){
-        convertColors_[c.r][c.g][c.b] = num++;
-    }
-
-    Image result = first_;
-    for (int y = 0; y < first_.getSize().y; ++y) {
-        for (int x = 0; x < first_.getSize().x; ++x) {
-            Color c = findColor(first_.getPixel(x, y));
-            result.setPixel(x, y, c);
-        }
-    }
-
-    return result;
+    return true;
 }
